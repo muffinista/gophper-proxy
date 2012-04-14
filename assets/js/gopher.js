f8d4f082b73aca8000b6a85a678645578dd3b3cd
Colin Mitchell <muffinista@gmail.com>
2012-04-13 21:47:57 -0400

    cleanup

--- assets/js/gopher.js
+++ assets/js/gopher.js
@@ -1,10 +1,30 @@
 (function( $ ){
 
-	//DirEntity ::= Type User_Name Tab Selector Tab Host Tab Port CR-LF
-	//1The QuIX Auditorium	1/audio	quix.us	70	+
+	// DirEntity ::= Type User_Name Tab Selector Tab Host Tab Port CR-LF
+	// 1The QuIX Auditorium	1/audio	quix.us	70	+
+
+	// regex for matching gopher entries. it didn't quite work in it's original form (see below)
     var entryPattern = /^(.)(.*?)\t(.*?)\t(.*?)\t(\d+).*/;
 
-	function gopher_parsedir (dirent) {
+	var entryTypes = {
+		info: { style: 'info' },
+		directory: { style: 'info' },
+		document: { style: 'document' },
+		binhex: { style: 'file' },
+		dosbinary: { style: 'file' },
+		uuencoded: { style: 'file' },
+		binary: { style: 'file' },
+		http: { style: 'http' },
+		image: { style: 'image' },
+		audio: { style: 'audio' },
+		unknown: { style: 'unknown' }
+	};
+
+	/**
+	 * borrowed from phpjs:
+	 * @see http://phpjs.org/functions/gopher_parsedir:833
+	 */
+	var gopher_parsedir = function(dirent) {
 		//var entryPattern = /^(.)(.*?)\t(.*?)\t(.*?)\t(.*?)\u000d\u000a$/;
 		// http://kevin.vanzonneveld.net
 		// +   original by: Brett Zamir (http://brett-zamir.me)
@@ -37,32 +57,38 @@
 		var type = entry[1];
 		switch (type) {
 		case 'i':
-			type = 255; // GOPHER_INFO
+			type = entryTypes.info;
 			break;
 		case '1':
-			type = 1; // GOPHER_DIRECTORY
+			type = entryTypes.directory;
 			break;
 		case '0':
-			type = 0; // GOPHER_DOCUMENT
+			type = entryTypes.document;
 			break;
 		case '4':
-			type = 4; // GOPHER_BINHEX
+			type = entryTypes.binhex;
 			break;
 		case '5':
-			type = 5; // GOPHER_DOSBINARY
+			type = entryTypes.dosbinary;
 			break;
 		case '6':
-			type = 6; // GOPHER_UUENCODED
+			type = entryTypes.uuencoded;
 			break;
 		case '9':
-			type = 9; // GOPHER_BINARY
+			type = entryTypes.binary;
 			break;
 		case 'h':
-			type = 254; // GOPHER_HTTP
+			type = entryTypes.html;
+			break;
+		case 'd':
+			type = entryTypes.image;
+			break;
+		case 's':
+			type = entryTypes.audio;
 			break;
 		default:
 			return {
-				type: -1,
+				type: entryTypes.unknown,
 				data: dirent
 			}; // GOPHER_UNKNOWN
 		}
@@ -73,73 +99,79 @@
 			host: entry[4],
 			port: entry[5]
 		};
-	};
+	},
+	 parseGopher = function(data) {
+		 var lines = [];
+		 data = data.split("\n");
+		 for ( var i = 0; i < data.length; i++ ) {
+			 lines.push(gopher_parsedir(data[i]));
+		 }
 
-	var parseGopher = function(data) {
-		var lines = [];
-		data = data.split("\n");
-		for ( var i = 0; i < data.length; i++ ) {
-			lines.push(gopher_parsedir(data[i]));
+		 return lines;
+	 },
+	 shouldRender = function(d) {
+		 var data = d.split("\n");
+		 return data[0].match(entryPattern) !== null;
+	 },
+	 isBinary = function(d) {
+		 return /[\x00-\x1F]/.test(d);
+	 };
+
+	/**
+	 * parse some gophertext and render some pretty HTML from it.
+	 *
+	 * d - data to parse. if this isn't specified, parse the contents of the target element
+	 */
+	$.fn.fromGopher = function(d) {
+		var data, entries;
+
+		if ( typeof(d) === "undefined" ) {
+			data = $(this).html().trim();
+		}
+		else {
+			data = d;
 		}
 
-		return lines;
-	};
-
-	var methods = {
-		init : function( options ) {
-
-		},
-		shouldRender : function( ) {
-			var data = $(this).html().trim().split("\n");
-			return data[0].match(entryPattern) !== null;
-		},
-		parse : function( ) {
-			var data = $(this).html().trim();
-			return parseGopher(data);
-		},
-		fromGopher : function(d) {
-			var data, entries;
-
-			if ( typeof(d) === "undefined" ) {
-				data = $(this).html().trim();
+		// make sure we should render -- if this doesn't look like gophertext,
+		// we'll just spit back the text itself
+		if ( ! shouldRender(data) ) {
+			if ( ! isBinary(data) ) {
+				// @todo add paragraphs/breaks
+				$(this).html(data);
 			}
 			else {
-				data = d;
+				$(this).html("Sorry, something went wrong there.");
 			}
+		}
+		else {
 			entries = parseGopher(data);
 
+			// reset the container
 			$(this).html("");
 
 			for ( var i = 0; i < entries.length; i++ ) {
 				var e = entries[i];
+
 				var href = "/" + e.host + e.path;
 				var text = e.title;
+				var type = e.type;
 				var result;
 
-				if ( e.path == "" ) {
+
+				// if there was no path, don't output a URL
+				if ( !e.path || e.path == "" ) {
 					result = text;
 				}
 				else {
-					result = $("<a />").attr("href", href).html(text);
+					result = $("<a />").
+						attr("href", href).
+						addClass("gopher-" + type.style).
+						html(text);
 				}
 
 				$(this).append(result).append("<br />");
 			}
-
 		}
+		return $(this);
 	};
-
-	$.fn.gopher = function( method ) {
-		// Method calling logic
-		if ( methods[method] ) {
-			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-		} else if ( typeof method === 'object' || ! method ) {
-			return methods.init.apply( this, arguments );
-		} else {
-			$.error( 'Method ' +  method + ' does not exist on jQuery.gopher' );
-		}
-	};
-
 })( jQuery );
-
-//https://raw.github.com/kvz/phpjs/master/functions/net-gopher/gopher_parsedir.js
