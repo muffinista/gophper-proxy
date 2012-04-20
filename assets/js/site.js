de060de9098e2556e4d887174cd4e8f2152c0700
Colin Mitchell <muffinista@gmail.com>
2012-04-19 20:04:36 -0400

    docs, updates

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
 
-	// Bind to StateChange Event
+	/**
+	 * on stateChange events, we will get the data from that page
+	 * (stored when loadGopherUri is complete), and re-render the
+	 * gopher output .
+	 */
     History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
         var state = History.getState(); // Note: We are using History.getState() instead of event.state
 		$("#gopher").html(state.data.data).fromGopher();
     });
 
+	/**
+	 * click handler for gopher menus. pass the URI along to the proxy
+	 * server, and handle the results.
+	 */
 	var loadGopherUri = function(u) {
 		$.ajax({
 			url: "/gopher",
@@ -15,16 +23,27 @@ $(document).ready(function() {
 				url: u
 			}
 		}).done(function ( data ) {
+
+			// hide the intro text if it is still there
+			$("#intro").hide();
+
+			// did we get valid data? if so, try and render it
 			if ( data.data ) {
-				// update browser url
+				// update browser url and store the data hash for
+				// later use. for now we'll set the title to be the
+				// URI as well
 				History.pushState(data, data.url, data.url);
 
+				// render the content
 				$("#gopher").html(data.data).fromGopher();
 			}
 			else {
 				// binary file (or something we can't render) -- redirect to source
 				window.location.replace(data.url);
 			}
+
+			// @todo handle errors
+
 		});
 	};
 
@@ -34,6 +53,7 @@ $(document).ready(function() {
 	});
 
 
+	// Handle existing URI on the browser's URL
 	if ( window.location.pathname != "/" ) {
 		loadGopherUri(window.location.pathname);
 	}
