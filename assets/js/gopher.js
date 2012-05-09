24a1bcf26ec6731b8ea14338add2fd03ebd75f81
Colin Mitchell <muffinista@gmail.com>
2012-05-08 21:01:28 -0400

    asset updates

--- assets/js/gopher.js
+++ assets/js/gopher.js
@@ -1,4 +1,4 @@
-function GopherParser() {};
+function GopherParser() {}
 
 /**
  *  regex for matching gopher entries. borrowed and modified from phpjs
@@ -15,19 +15,19 @@ GopherParser.prototype.entryPattern = /^(.)(.*?)\t(.*?)\t(.*?)\t(\d+).*/;
  *   the Twitter Bootstrap library
  */
 GopherParser.prototype.entryTypes = {
-	info: { link : false },
-	error: { link : false, icon : 'icon-exclamation-sign' },
-	directory: { link : true, icon : 'icon-folder-open' },
-	document: { link : true, icon : 'icon-file' },
-	binhex: { link : true, icon : 'icon-download-alt' },
-	dosbinary: { link : true, icon : 'icon-download-alt' },
-	uuencoded: { link : true, icon : 'icon-download-alt' },
-	binary: { link : true, icon : 'icon-download-alt' },
-	html: { link : true, icon : 'icon-bookmark' },
-	search: { link : true, form: true, icon : 'icon-search' },
-	image: { link : true, icon : 'icon-picture' },
-	audio: { link : true, icon : 'icon-music' },
-	unknown: { link : true, icon : 'icon-question-sign' }
+  info: { link : false },
+  error: { link : false, icon : 'icon-exclamation-sign' },
+  directory: { link : true, icon : 'icon-folder-open' },
+  document: { link : true, icon : 'icon-file' },
+  binhex: { link : true, icon : 'icon-download-alt' },
+  dosbinary: { link : true, icon : 'icon-download-alt' },
+  uuencoded: { link : true, icon : 'icon-download-alt' },
+  binary: { link : true, icon : 'icon-download-alt' },
+  html: { link : true, icon : 'icon-bookmark' },
+  search: { link : true, form: true, icon : 'icon-search' },
+  image: { link : true, icon : 'icon-picture' },
+  audio: { link : true, icon : 'icon-music' },
+  unknown: { link : true, icon : 'icon-question-sign' }
 };
 
 
@@ -37,8 +37,8 @@ GopherParser.prototype.entryTypes = {
  * @return true if we should parse as gopher menu, false otherwise
  */
 GopherParser.prototype.shouldRender = function(d) {
-	var data = d.split("\n");
-	return data[0].match(this.entryPattern) !== null;
+  var data = d.split("\n");
+  return data[0].match(this.entryPattern) !== null;
 };
 
 /**
@@ -47,15 +47,15 @@ GopherParser.prototype.shouldRender = function(d) {
  * @return array of objects which represent the data of the menu
  */
 GopherParser.prototype.parseGopher = function(data) {
-	var lines = [];
-	data = data.split("\n");
-	for ( var i = 0; i < data.length; i++ ) {
-		if ( data[i] != "." ) {
-			lines.push(this.parseEntry(data[i]));
-		}
-	}
+  var lines = [];
+  data = data.split("\n");
+  for ( var i = 0; i < data.length; i++ ) {
+    if ( data[i] != "." ) {
+      lines.push(this.parseEntry(data[i]));
+    }
+  }
 
-	return lines;
+  return lines;
 };
 
 
@@ -65,46 +65,34 @@ GopherParser.prototype.parseGopher = function(data) {
  * @return entry type object
  */
 GopherParser.prototype.getType = function(t) {
-	switch (t) {
-	case 'i':
-		return this.entryTypes.info;
-		break;
-	case '3':
-		return this.entryTypes.error;
-		break;
-	case '1':
-		return this.entryTypes.directory;
-		break;
-	case '0':
-		return this.entryTypes.document;
-		break;
-	case '4':
-		return this.entryTypes.binhex;
-		break;
-	case '5':
-		return this.entryTypes.dosbinary;
-		break;
-	case '6':
-		return this.entryTypes.uuencoded;
-		break;
-	case '7':
-		return this.entryTypes.search;
-		break;
-	case '9':
-		return this.entryTypes.binary;
-		break;
-	case 'h':
-		return this.entryTypes.html;
-		break;
-	case 'd':
-		return this.entryTypes.image;
-		break;
-	case 's':
-		return this.entryTypes.audio;
-		break;
-	default:
-		return this.entryTypes.unknown;
-	}
+  switch (t) {
+  case 'i':
+    return this.entryTypes.info;
+  case '3':
+    return this.entryTypes.error;
+  case '1':
+    return this.entryTypes.directory;
+  case '0':
+    return this.entryTypes.document;
+  case '4':
+    return this.entryTypes.binhex;
+  case '5':
+    return this.entryTypes.dosbinary;
+  case '6':
+    return this.entryTypes.uuencoded;
+  case '7':
+    return this.entryTypes.search;
+  case '9':
+    return this.entryTypes.binary;
+  case 'h':
+    return this.entryTypes.html;
+  case 'd':
+    return this.entryTypes.image;
+  case 's':
+    return this.entryTypes.audio;
+  default:
+    return this.entryTypes.unknown;
+  }
 };
 
 /**
@@ -116,20 +104,20 @@ GopherParser.prototype.getType = function(t) {
  * @return object
  */
 GopherParser.prototype.parseEntry = function(dirent) {
-	var entry = dirent.match(this.entryPattern);
+  var entry = dirent.match(this.entryPattern);
 
-	// parse error
-	if (entry === null) {
-		return {};
-	}
+  // parse error
+  if (entry === null) {
+    return {};
+  }
 
-	return {
-		type: this.getType(entry[1]),
-		title: entry[2],
-		path: entry[3],
-		host: entry[4],
-		port: entry[5]
-	};
+  return {
+    type: this.getType(entry[1]),
+    title: entry[2],
+    path: entry[3],
+    host: entry[4],
+    port: entry[5]
+  };
 
 };
 
@@ -141,121 +129,121 @@ GopherParser.prototype.parseEntry = function(dirent) {
  * @return a href that will request this menu entry via the proxy
  */
 GopherParser.prototype.entryToLink = function(e) {
-	var href = "/" + e.host;
+  var href = "/" + e.host;
 
-	// add the port if needed
-	if ( e.port != 70 ) {
-		href = href + ":" + e.port;
-	}
+  // add the port if needed
+  if ( e.port != 70 ) {
+    href = href + ":" + e.port;
+  }
 
-	// clean up the path a bit, make sure there's always a slash
-	if ( e.path && e.path[0] != "/" ) {
-		e.path = "/" + e.path;
-	}
+  // clean up the path a bit, make sure there's always a slash
+  if ( e.path && e.path[0] != "/" ) {
+    e.path = "/" + e.path;
+  }
 
 
-	href = href + e.path;
+  href = href + e.path;
 
-	return href;
+  return href;
 };
 
 (function( $ ){
-	/**
-	 * convert newlines to breaks
-	 * @see http://phpjs.org/functions/nl2br:480
-	 */
-	function nl2br (str, is_xhtml) {
-		var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
-		return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
-	}
+  /**
+   * convert newlines to breaks
+   * @see http://phpjs.org/functions/nl2br:480
+   */
+  function nl2br (str, is_xhtml) {
+    var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
+    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+  }
 
-	/**
-	 * parse some gophertext and render some pretty HTML from it.
-	 *
-	 * d - data to parse. if this isn't specified, parse the contents of the target element
-	 */
-	$.fn.fromGopher = function(d) {
-		var data, entries;
-		var parser = new GopherParser();
+  /**
+   * parse some gophertext and render some pretty HTML from it.
+   *
+   * d - data to parse. if this isn't specified, parse the contents of the target element
+   */
+  $.fn.fromGopher = function(d) {
+    var data, entries;
+    var parser = new GopherParser();
 
-		// if we didn't get any incoming data, use the content of our target
-		if ( typeof(d) === "undefined" ) {
-			data = $(this).html().trim();
-		}
-		else {
-			data = d;
-		}
+    // if we didn't get any incoming data, use the content of our target
+    if ( typeof(d) === "undefined" ) {
+      data = $(this).html().trim();
+    }
+    else {
+      data = d;
+    }
 
-		// make sure we should render -- if this doesn't look like gophertext,
-		// we'll just spit back the text itself
-		if ( ! parser.shouldRender(data) ) {
-			// if we're specifying preformatted text via CSS, then
-			// don't add any newlines to the output
-			if ( $(this).css("white-space") == "pre" ) {
-				$(this).html(data);
-			}
-			else {
-				$(this).html(nl2br(data));
-			}
-		}
-		else {
-			entries = parser.parseGopher(data);
+    // make sure we should render -- if this doesn't look like gophertext,
+    // we'll just spit back the text itself
+    if ( ! parser.shouldRender(data) ) {
+      // if we're specifying preformatted text via CSS, then
+      // don't add any newlines to the output
+      if ( $(this).css("white-space") == "pre" ) {
+        $(this).html(data);
+      }
+      else {
+        $(this).html(nl2br(data));
+      }
+    }
+    else {
+      entries = parser.parseGopher(data);
 
-			// reset the container
-			$(this).html("");
+      // reset the container
+      $(this).html("");
 
-			for ( var i = 0; i < entries.length; i++ ) {
-				var e = entries[i];
+      for ( var i = 0; i < entries.length; i++ ) {
+        var e = entries[i];
 
-				// don't freak out if there's no valid type, just skip the line
-				if ( ! e.type ) {
-					continue;
-				}
+        // don't freak out if there's no valid type, just skip the line
+        if ( ! e.type ) {
+          continue;
+        }
 
-				var href = parser.entryToLink(e);
-				var text = e.title;
-				var type = e.type;
+        var href = parser.entryToLink(e);
+        var text = e.title;
+        var type = e.type;
 
-				var result;
-				var icon = "";
+        var result;
+        var icon = "";
 
 
-				//
-				// generate a form for search entries
-				//
-				if ( typeof(type.form) !== "undefined" && type.form == true ) {
-					result = $("<form method='post' />").
-						attr("action", href).
-						addClass("form-inline");
+        //
+        // generate a form for search entries
+        //
+        if ( typeof(type.form) !== "undefined" && type.form === true ) {
+          result = $("<form method='post' />").
+            attr("action", href).
+            addClass("form-inline");
 
-					var button = $("<button />").attr("type", "submit").addClass("btn").html("Go!");
-					$(result).
-						append("<input name='text' class='span3' placeholder='input' />").
-						append(button);
+          var button = $("<button />").attr("type", "submit").addClass("btn").html("Go!");
+          $(result).
+            append("<input name='text' class='span3' placeholder='input' />").
+            append(button);
 
-				}
+        }
 
-				// if there was no path, don't output a URL
-				else if ( type.link == false ) {
-					result = text;
-				}
+        // if there was no path, don't output a URL
+        else if ( type.link === false ) {
+          result = text;
+        }
 
-				// output a link
-				else {
-					result = $("<a />").
-						attr("href", href).
-						html(text);
-				}
+        // output a link
+        else {
+          result = $("<a />").
+            attr("href", href).
+            html(text);
+        }
 
-				// if we have an icon class, add it here
-				if ( type.icon ) {
-					icon = $("<i />").addClass(type.icon).append("&nbsp;");
-				}
+        // if we have an icon class, add it here
+        if ( type.icon ) {
+          icon = $("<i />").addClass(type.icon).append("&nbsp;");
+        }
 
-				// add the output!
-				$(this).append(icon).append(result).append("<br />");
-			}
-		}
-		return $(this);
-	};
+        // add the output!
+        $(this).append(icon).append(result).append("<br />");
+      }
+    }
+    return $(this);
+  };
 })( jQuery );
--- assets/js/site.js
+++ assets/js/site.js
@@ -1,196 +1,196 @@
 $(document).ready(function() {
-	/**
-	 * on stateChange events, we will get the data from that page
-	 * (stored when loadGopherUri is complete), and re-render the
-	 * gopher output.
-	 */
+  /**
+   * on stateChange events, we will get the data from that page
+   * (stored when loadGopherUri is complete), and re-render the
+   * gopher output.
+   */
   History.Adapter.bind(window, 'statechange', function() {
     var state = History.getState();
 
-		if ( state.data.data ) {
-			$("#gopher").html(state.data.data).fromGopher();
+    if ( state.data.data ) {
+      $("#gopher").html(state.data.data).fromGopher();
 
-			// update our breadcrumb nav
-			updateBreadcrumb(state.data.url);
-		}
-		else {
-			$("#gopher,#breadcrumb").fadeOut().html("");
-			$("#intro").fadeIn();
-		}
-	});
-
-
-	/**
-	 * very simple methods to manage a 'please wait' spinner
-	 */
-	var spinner = {
-		spin : function() {
-			$("#spinner img").fadeIn();
-		},
-		unspin : function() {
-			$("#spinner img").fadeOut();
-		}
-	};
+      // update our breadcrumb nav
+      updateBreadcrumb(state.data.url);
+    }
+    else {
+      $("#gopher,#breadcrumb").fadeOut().html("");
+      $("#intro").fadeIn();
+    }
+  });
 
 
-	/**
-	 * output a breadcrumb which will just split the current URI and
-	 * link to each level
-	 */
-	var updateBreadcrumb = function(url) {
-		var bc = unescape(url).split("/");
-		var crumb = "";
-		var li;
-
-		$("#breadcrumb").html("<ul class='breadcrumb' />");
-
-		for ( var i = 0; i < bc.length; i++ ) {
-			crumb = crumb + bc[i] + "/";
-
-			li = $("<li />");
-			if ( i + 1 == bc.length ) {
-				$(li).addClass("active");
-			}
-
-			$(li).html($("<a />").attr("href", crumb).html(bc[i]));
-
-			if ( i + 1 < bc.length ) {
-				$(li).append($("<span />").addClass("divider").html("/"));
-			}
-
-			$(".breadcrumb").append(li);
-		}
-	};
+  /**
+   * very simple methods to manage a 'please wait' spinner
+   */
+  var spinner = {
+    spin : function() {
+      $("#spinner img").fadeIn();
+    },
+    unspin : function() {
+      $("#spinner img").fadeOut();
+    }
+  };
 
 
-	/**
-	 * click handler for gopher menus. pass the URI along to the proxy
-	 * server, and handle the results.
-	 */
-	var loadGopherUri = function(params) {
-		var data = {};
+  /**
+   * output a breadcrumb which will just split the current URI and
+   * link to each level
+   */
+  var updateBreadcrumb = function(url) {
+    var bc = unescape(url).split("/");
+    var crumb = "";
+    var li;
 
-		if ( typeof(params) === "string" ) {
-			data.url = params;
-		}
-		else {
-			data.url = params.url;
-			data.input = params.input;
-		}
+    $("#breadcrumb").html("<ul class='breadcrumb' />");
 
-			// hide the intro text if it is still there
-			$("#intro").hide();
-			$("#gopher").show();
+    for ( var i = 0; i < bc.length; i++ ) {
+      crumb = crumb + bc[i] + "/";
 
-		var displayError = function() {
-			spinner.unspin();
-			$("#gopher").show().html("Sorry, there was a problem with your request, please try again.");
-			$('html,body').animate({scrollTop: $("#gopher").offset().top}, 'slow');
-		};
+      li = $("<li />");
+      if ( i + 1 == bc.length ) {
+        $(li).addClass("active");
+      }
 
-		spinner.spin();
+      $(li).html($("<a />").attr("href", crumb).html(bc[i]));
 
-		$.ajax({
-			url: "/gopher",
-			type: 'post',
-			dataType: 'json',
-			data: data,
-			error : function() {
-				displayError();
-			}
-		}).done(function ( data ) {
+      if ( i + 1 < bc.length ) {
+        $(li).append($("<span />").addClass("divider").html("/"));
+      }
 
-			spinner.unspin();
-
-			if ( typeof(params.onComplete) !== "undefined" ) {
-				params.onComplete();
-			}
-
-			// did we get valid data? if not, display the error
-			if ( data.error ) {
-				$("#gopher").html(data.error);
-			}
-
-			//
-			// we got data, let's render it
-			//
-			else if ( data.data ) {
-
-				// update browser url and store the data hash for
-				// later use. for now we'll set the title to be the
-				// URI as well
-				History.pushState(data, data.url, data.url);
-
-				// update the URI in the header
-				$("form input[name=uri]").val(data.url);
-
-				// update our breadcrumb nav
-				updateBreadcrumb(data.url);
-
-				// render the content
-				$("#gopher").html(data.data).fromGopher();
-
-				// scroll to the top of the page
-				$('html, body').animate({ scrollTop: 0 }, 0);
-
-				// Google Analytics support
-		    if ( window._gaq ) {
-		      _gaq.push(['_trackPageview']);
-		    }
-			}
+      $(".breadcrumb").append(li);
+    }
+  };
 
 
-			//
-			// at this point, we probably just have a URL for a file, link to it
-			//
-			else {
-				// if it's an image, load in a colorbox
-				if ( data.image ) {
-					$.colorbox({photo: true, href: data.url});
-				}
-				else {
-					// binary file (or something we can't render) -- redirect to cached copy on proxy server
-					window.location.replace(data.url);
-				}
-			}
+  /**
+   * click handler for gopher menus. pass the URI along to the proxy
+   * server, and handle the results.
+   */
+  var loadGopherUri = function(params) {
+    var data = {};
 
-		}).fail(function(jqXHR, textStatus) {
-			displayError();
-		});
+    if ( typeof(params) === "string" ) {
+      data.url = params;
+    }
+    else {
+      data.url = params.url;
+      data.input = params.input;
+    }
 
-	};
+      // hide the intro text if it is still there
+      $("#intro").hide();
+      $("#gopher").show();
 
-	/**
-	 * handle clicks on gopher selectors
-	 */
-	$("#gopher,#breadcrumb,#intro .as-html").on("click", "a", function() {
-		loadGopherUri({
-			url : $(this).attr("href")
-		});
-		return false;
-	}).on("submit", "form", function() {
-		loadGopherUri({
-			url : $(this).attr("action"),
-			input : $(this).find("input").val()
-		});
-		return false;
-	});;
+    var displayError = function() {
+      spinner.unspin();
+      $("#gopher").show().html("Sorry, there was a problem with your request, please try again.");
+      $('html,body').animate({scrollTop: $("#gopher").offset().top}, 'slow');
+    };
+
+    spinner.spin();
+
+    $.ajax({
+      url: "/gopher",
+      type: 'post',
+      dataType: 'json',
+      data: data,
+      error : function() {
+        displayError();
+      }
+    }).done(function ( data ) {
+
+      spinner.unspin();
+
+      if ( typeof(params.onComplete) !== "undefined" ) {
+        params.onComplete();
+      }
+
+      // did we get valid data? if not, display the error
+      if ( data.error ) {
+        $("#gopher").html(data.error);
+      }
+
+      //
+      // we got data, let's render it
+      //
+      else if ( data.data ) {
+
+        // update browser url and store the data hash for
+        // later use. for now we'll set the title to be the
+        // URI as well
+        History.pushState(data, data.url, data.url);
+
+        // update the URI in the header
+        $("form input[name=uri]").val(data.url);
+
+        // update our breadcrumb nav
+        updateBreadcrumb(data.url);
+
+        // render the content
+        $("#gopher").html(data.data).fromGopher();
+
+        // scroll to the top of the page
+        $('html, body').animate({ scrollTop: 0 }, 0);
+
+        // Google Analytics support
+        if ( window._gaq ) {
+          _gaq.push(['_trackPageview']);
+        }
+      }
 
 
-	/**
-	 * handle URI form submission
-	 */
-	$("form.gopher-uri").on("submit", function() {
-		loadGopherUri($(this).find("input[name=uri]").val());
-		return false;
-	});
+      //
+      // at this point, we probably just have a URL for a file, link to it
+      //
+      else {
+        // if it's an image, load in a colorbox
+        if ( data.image ) {
+          $.colorbox({photo: true, href: data.url});
+        }
+        else {
+          // binary file (or something we can't render) -- redirect to cached copy on proxy server
+          window.location.replace(data.url);
+        }
+      }
+
+    }).fail(function(jqXHR, textStatus) {
+      displayError();
+    });
+
+  };
+
+  /**
+   * handle clicks on gopher selectors
+   */
+  $("#gopher,#breadcrumb,#intro .as-html").on("click", "a", function() {
+    loadGopherUri({
+      url : $(this).attr("href")
+    });
+    return false;
+  }).on("submit", "form", function() {
+    loadGopherUri({
+      url : $(this).attr("action"),
+      input : $(this).find("input").val()
+    });
+    return false;
+  });
 
 
-	/**
-	 * Handle existing URI on the browser's URL -- we'll only do this if the intro is hidden, 
-	 * and there's something to load. We'll add the 'hide' class to the output in home.html to trigger this.
-	 */
-	if ( window.location.pathname != "/" && $("#intro").hasClass("hide") ) {
-		loadGopherUri(unescape(window.location.pathname));
-	}
+  /**
+   * handle URI form submission
+   */
+  $("form.gopher-uri").on("submit", function() {
+    loadGopherUri($(this).find("input[name=uri]").val());
+    return false;
+  });
+
+
+  /**
+   * Handle existing URI on the browser's URL -- we'll only do this if the intro is hidden, 
+   * and there's something to load. We'll add the 'hide' class to the output in home.html to trigger this.
+   */
+  if ( window.location.pathname != "/" && $("#intro").hasClass("hide") ) {
+    loadGopherUri(unescape(window.location.pathname));
+  }
 });
