69e9274e6e934f7964bc6608f86d4c1a5010c884
Colin Mitchell <muffinista@gmail.com>
2022-07-01 15:58:08 -0400

    Add error output

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
-error_reporting(0);
+error_reporting(1);
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-require __DIR__ . '/vendor/autoload.php';
+// require __DIR__ . '/vendor/autoload.php';
+require 'vendor/autoload.php';
 
 require 'lib/GopherGetter.php';
 
